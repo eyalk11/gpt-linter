bb02e8b9643c0bd21e246df463d6e42e403209b1
LAPTOP-G43MA5MO\ekarni <eyalk5@gmail.com>
2023-07-21 22:12:53 +0300

    another version, with MANIFEST

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ with open('requirements.txt') as f:
 import pkg_resources
 import setuptools
 import pathlib
-desc = 'Python minor issue resolver (mypy) using gpt by openai! '
+desc = 'Python minor issue resolver (mypy) using gpt by openai!'
+long_desc = 'Solves mypy errors using guidance and GPT api. It runs mypy on targeted file and then uses gpt to try to fix the issues (espcially good for minor nagging issues). Displays a diff file for the required changes and ask you if you want to apply.'
 with pathlib.Path('requirements.txt').open() as requirements_txt:
     install_requires = [
         str(requirement)
@@ -14,7 +15,7 @@ with pathlib.Path('requirements.txt').open() as requirements_txt:
 
 setup(
     name='mypy-gpt',
-    version='1.0.1',
+    version='1.0.2',
     packages=['mypy_gpt'],
     url='https://github.com/eyalk11/mypy-gpt',
     license=' AGPL-3.0 license',
